f10087d438bb92a0de73ad753f27469a87998b2f
g_ogaki <g_ogaki@r.recruit.co.jp>
2021-06-26 21:17:01 +0900

    add app/Dockerfile & rename

--- app/run.py
+++ app/run.py
@@ -1,15 +1,22 @@
 from flask import Flask, render_template
 from pymysql import connect, cursors
+from time import sleep
 
 app = Flask(__name__)
-connection = connect(
-    host="db",
-    port=3306,
-    user="root",
-    password="hoge",
-    database="test",
-    cursorclass=cursors.DictCursor,
-)
+while True:
+    try:
+        connection = connect(
+            host="db",
+            port=3306,
+            user="root",
+            password="hoge",
+            database="test",
+            cursorclass=cursors.DictCursor,
+        )
+        break
+    except:
+        print("Couldn't connect to database. trying to reconnect in 3 seconds")
+        sleep(3)
 cursor = connection.cursor()
 
 @app.route("/")
